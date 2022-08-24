e7b06b1079ad09421ef2f059772989c059201e30
Blackwidow-sudo <bob_merlin92@web.de>
2022-08-24 22:08:15 +0200

    setup backend for serving SPA

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -4,7 +4,7 @@
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
         
-        @vite('resources/ts/app.ts')
+        @vite(['resources/ts/app.ts', 'resources/sass/app.scss'])
 
         <title>SveLaVi</title>
     </head>
