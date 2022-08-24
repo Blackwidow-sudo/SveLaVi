fab39fb8f1071e8ada00ecd19334e58c388bb570
Blackwidow-sudo <bob_merlin92@web.de>
2022-08-24 22:03:30 +0200

    update vite.config.js & add svelte.config.mjs

--- vite.config.js
+++ vite.config.js
@@ -1,21 +1,24 @@
-import { defineConfig } from "vite";
-import laravel from "laravel-vite-plugin";
-import { svelte } from "@sveltejs/vite-plugin-svelte";
-import sveltePreprocess from "svelte-preprocess";
+import { defineConfig } from 'vite'
+import laravel from 'laravel-vite-plugin'
+import { svelte } from '@sveltejs/vite-plugin-svelte'
+import sveltePreprocess from 'svelte-preprocess'
+import path from 'path'
 
 export default defineConfig({
-    plugins: [
-        laravel({
-            input: ["resources/css/app.css", "resources/ts/app.ts"],
-            refresh: true,
-        }),
-        svelte({
-            preprocess: sveltePreprocess(),
-        }),
-    ],
-    resolve: {
-        alias: {
-            "@": "/resources/ts",
-        },
+  plugins: [
+    laravel({
+      input: ['resources/sass/app.scss', 'resources/ts/app.ts'],
+      refresh: true,
+    }),
+    svelte({
+      preprocess: sveltePreprocess(),
+    }),
+  ],
+  resolve: {
+    alias: {
+      '@': path.resolve(__dirname, 'resources/ts'),
+      '~': path.resolve(__dirname, 'resources/sass'),
+      '~bootstrap': path.resolve(__dirname, 'node_modules/bootstrap'),
     },
-});
+  },
+})
