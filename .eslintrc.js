b8c596e2efdee6a3b5fafab14bbf931154e1409b
Blackwidow-sudo <bob_merlin92@web.de>
2022-08-24 22:04:22 +0200

    add configuration for eslint and prettier

---
+++ .eslintrc.js
@@ -0,0 +1,29 @@
+module.exports = {
+  env: {
+    browser: true,
+    es6: true,
+    node: true,
+  },
+  extends: ['eslint:recommended', 'plugin:@typescript-eslint/recommended'],
+  overrides: [
+    {
+      files: ['*.svelte'],
+      processor: 'svelte3/svelte3',
+    },
+  ],
+  parser: '@typescript-eslint/parser',
+  parserOptions: {
+    ecmaVersion: 2019,
+    sourceType: 'module',
+  },
+  plugins: ['@typescript-eslint', 'svelte3'],
+  settings: {
+    'svelte3/typescript': () => require('typescript'),
+  },
+  rules: {
+    'indent': ['error', 2],
+    'linebreak-style': ['error', 'unix'],
+    'quotes': ['error', 'single'],
+    'semi': ['error', 'never'],
+  },
+}
